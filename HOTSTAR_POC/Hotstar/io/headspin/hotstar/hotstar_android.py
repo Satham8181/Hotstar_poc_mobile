ae98563577caf43414ce5dba4e1e80e4f0e966df
NeethuKrishnaC <77262362+NeethuKrishnaC@users.noreply.github.com>
2023-08-05 01:00:01 +0530

    Update hotstar_android.py

--- HOTSTAR_POC/Hotstar/io/headspin/hotstar/hotstar_android.py
+++ HOTSTAR_POC/Hotstar/io/headspin/hotstar/hotstar_android.py
@@ -242,7 +242,8 @@ class BupaAndroidTest(unittest.TestCase):
         movie_image  = self.driver.find_element(MBy.ACCESSIBILITY_ID, "West Is Best")
         self.kpi_labels[kpi_names.DETAILS_PAGE_LOAD_TIME]['start'] = int(round(time.time() * 1000)) + 4000
         movie_image.click()
-        watch = self.driver.find_element(MBy.XPATH, '//*[@resource-id="Test Tag Button Layout Start Icon"]')
+        #watch = self.driver.find_element(MBy.XPATH, '//*[@resource-id="Test Tag Button Layout Start Icon"]')
+        watch = self.driver.find_element(MBy.XPATH, '//*[@resource-id="tag_label_button_layout"]')
         self.kpi_labels[kpi_names.DETAILS_PAGE_LOAD_TIME]['end'] = int(round(time.time() * 1000)) + 500
         sleep(2)
         self.kpi_labels[kpi_names.VIDEO_LOAD_TIME]['start'] = int(round(time.time() * 1000)) + 5000
