d51086d1e10182dd0fffaa9b585709d2a550aa13
NeethuKrishnaC <77262362+NeethuKrishnaC@users.noreply.github.com>
2023-08-07 17:39:08 +0530

    Update hotstar_android.py

--- HOTSTAR_POC/Hotstar/io/headspin/hotstar/hotstar_android.py
+++ HOTSTAR_POC/Hotstar/io/headspin/hotstar/hotstar_android.py
@@ -264,7 +264,7 @@ class BupaAndroidTest(unittest.TestCase):
         # except:
         watch = self.driver.find_element(MBy.XPATH, '//*[@resource-id="Test Tag Button Layout Start Icon"]')
         self.kpi_labels[kpi_names.DETAILS_PAGE_LOAD_TIME]['end'] = int(round(time.time() * 1000)) + 500
-        #sleep(5)
+        sleep(3)
         watch = self.driver.find_element(MBy.XPATH, '//*[@resource-id="Test Tag Button Layout Start Icon"]')
         self.kpi_labels[kpi_names.VIDEO_LOAD_TIME]['start'] = int(round(time.time() * 1000)) + 5000
         watch.click()
